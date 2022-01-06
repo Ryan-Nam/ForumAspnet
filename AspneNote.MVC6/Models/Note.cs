f47d247a68746c5dca185e707b35511e02861c7b
Admin <Admin@IT-PC12>
2022-01-07 00:21:03 +1100

    Add project files.

---
+++ AspneNote.MVC6/Models/Note.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspneNote.MVC6.Models
+{
+    public class Note
+    {
+        /// <summary>
+        /// 게시물 번호 
+        /// </summary>
+        [Key]
+        public int NoteNo { get; set; }
+
+        /// <summary>
+        /// 게시물 제목 
+        /// </summary>
+        [Required] // Not Null
+        public string NoteTitle { get; set; }
+
+        /// <summary>
+        /// 게시물 내용 
+        /// </summary>
+        [Required] // Not Null
+        public int NoteContents { get; set; }
+
+        /// <summary>
+        /// 작성자 번호
+        /// </summary>
+        [Required] // Not Null
+        public int UserNo { get; set; }
+
+        [ForeignKey("USerNo")]
+        public virtual User User { get; set; } //lazy loading 을 위해 virtual 을 넣어두자.
+
+
+
+    }
+}
---
+++ AspneNote.MVC6/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspneNote.MVC6.Models
+{
+    public class User
+    {
+        /// <summary>
+        /// 사용자 번호
+        /// </summary>
+        [Key] // PK 설정
+        public int UserNo { get; set; }
+
+        /// <summary>
+        /// 사용자 이름
+        /// </summary>
+        [Required] // Not Null
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 사용자 ID
+        /// </summary>
+        [Required] // Not Null
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// 사용자 비밀번호
+        /// </summary>
+        [Required] // Not Null
+        public string UserPassword { get; set; }
+
+    }
+}
